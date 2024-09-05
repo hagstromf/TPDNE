448d92a9e4403ad3b261b81497d688b0374cc4a9
Fredrik Hagstrom <hagstromf@gmail.com>
2024-09-05 16:15:05 +0300

    Implemented path length regularization. Also cleaned up DiscriminatorLoss code

--- training/loss.py
+++ training/loss.py
@@ -2,101 +2,95 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import numpy as np
+
 from utils.constants import REAL_LABEL, FAKE_LABEL
 
 
 class GeneratorLoss(nn.Module):
-    def __init__(self, pl_gamma, pl_beta=0.99):
-
+    def __init__(self, pl_weight=2.0, pl_beta=0.99):
         super().__init__()
 
-        self.pl_gamma = pl_gamma
+        self.pl_weight = pl_weight
         self.pl_beta = pl_beta
-        self.pl_ema = torch.zeros([], dtype=torch.float32)
+        self.pl_ema = torch.zeros([])
+        self.t = torch.ones([])
 
-    # TODO: Implement path length regularization
-    def pl_reg(self):
-        print(self.pl_ema)
-        self.pl_ema += 1
-        print(self.pl_ema)
+    def pl_reg(self, fake_images, ws):
+        *_, img_H, img_W = fake_images.shape
+        y = torch.randn_like(fake_images) / np.sqrt(img_H * img_W)
 
-    def forward(self, netD, fake_images, do_reg=False):
+        grad = torch.autograd.grad(fake_images * y,
+                                   ws,
+                                   grad_outputs=torch.ones_like(fake_images),
+                                   create_graph=True)[0]
+
+        norm = grad.square().sum(2).mean(0).sqrt()
+
+        self.pl_ema = self.pl_beta * self.pl_ema + (1 - self.pl_beta) * norm.detach().mean()
+        pl_ema_hat = self.pl_ema / (1 - self.pl_beta**self.t)
+
+        pl_penalty = (norm - pl_ema_hat).square().mean()
+     
+        return self.pl_weight * pl_penalty
+
+    def forward(self, netD, fake_images, ws, do_reg=False):
         batch_size = fake_images.shape[0]
         targets = REAL_LABEL * torch.ones((batch_size, 1)).to(fake_images.device)
 
-        output = netD(fake_images)
-
+        output = F.sigmoid(netD(fake_images))
         loss = F.binary_cross_entropy(output, targets)
 
+        pl_penalty = 0
         if do_reg:
-            self.pl_reg()
+            pl_penalty = self.pl_reg(fake_images, ws)
 
-        return loss
+        return loss, pl_penalty
     
 
 class DiscriminatorLoss(nn.Module):
-    def __init__(self,
-                 r1_gamma=10,
-                 r1_interval=16):
-
+    def __init__(self, r1_gamma=10):
         super().__init__()
 
         self.r1_gamma = r1_gamma
-        self.r1_interval = r1_interval
 
     def r1_reg(self, real_images, logits_real):
-        # print(real_images.requires_grad)
-        # print(logits_real.requires_grad)
         grad = torch.autograd.grad(logits_real,
                                    real_images,
                                    grad_outputs=torch.ones_like(logits_real),
                                    create_graph=True)[0]
-        # print(grad.shape)
+
         penalty = grad.square().sum([1, 2, 3])
         penalty = self.r1_gamma / 2 * grad.square().mean()
-        # print(penalty.shape)
+
         return penalty
     
     def forward(self, netD, real_images, fake_images, do_reg=False):
         batch_size = real_images.shape[0]
     
         real_images_tmp = real_images.detach().requires_grad_(do_reg)
+        fake_images_tmp = fake_images.detach()
 
         targets_real = REAL_LABEL * torch.ones((batch_size, 1)).to(real_images_tmp.device)
-        targets_fake = FAKE_LABEL * torch.ones((batch_size, 1)).to(fake_images.device)
+        targets_fake = FAKE_LABEL * torch.ones((batch_size, 1)).to(fake_images_tmp.device)
         
         logits_real = netD(real_images_tmp)
-        logits_fake = netD(fake_images)
-        # print(logits_real.shape)
-        # print(logits_real)
+        logits_fake = netD(fake_images_tmp)
 
         output_real = F.sigmoid(logits_real)
         output_fake = F.sigmoid(logits_fake)
-        # print(output_real.shape)
-        # print(output_real)
-        
+
         D_real = torch.mean(output_real).to(real_images_tmp.device)
-        D_fake = torch.mean(output_fake).to(fake_images.device)
-        # print(D_real.shape)
-        # print(D_real)
-        
+        D_fake = torch.mean(output_fake).to(fake_images_tmp.device)
+
         loss_real = F.binary_cross_entropy(output_real, targets_real)
         loss_fake = F.binary_cross_entropy(output_fake, targets_fake)
-        # print(loss_real.shape)
-        # print(loss_real)
-        # print()
 
         r1_penalty = 0
+        # r1_penalty = torch.zeros([]).to(real_images_tmp.device)
         if do_reg:
             r1_penalty = self.r1_reg(real_images_tmp, logits_real)
-        # print(r1_penalty.shape)
-        # print(r1_penalty)
 
         loss = loss_real + loss_fake
-        # loss = loss_real + r1_penalty + loss_fake
-        # print(loss.shape)
-        # print(loss)
         
         return loss, r1_penalty, D_real, D_fake
-        # return loss_real, loss_fake, r1_penalty, D_real, D_fake
-        # return d_loss_real, D_real, d_loss_fake, D_fake
