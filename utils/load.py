db58a162cafb4649906d0bfdd38a34648cc87427
Fredrik Hagstrom <hagstromf@gmail.com>
2024-10-03 20:15:42 +0300

    Added option to ignore return full data set in one dataloader instead of train test split

--- utils/load.py
+++ utils/load.py
@@ -3,7 +3,7 @@ from torchvision import datasets
 from torchvision.transforms import v2, functional as F
 
 
-def load_images(path, res, batch_size=32, test_size=0.1):
+def load_images(path, res, batch_size=32, test_size=None):
     transform = v2.Compose([v2.Resize((res, res)),
                             v2.ToImage(),
                             v2.ToDtype(torch.float32, scale=True),
@@ -12,12 +12,16 @@ def load_images(path, res, batch_size=32, test_size=0.1):
     
     dataset = datasets.ImageFolder(path, transform=transform)
 
+    if test_size is None:
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
+        return dataloader, None
+
     n = len(dataset) 
     n_test = int(test_size * n)  
-    test_set = torch.utils.data.Subset(dataset, range(n_test)) 
-    train_set = torch.utils.data.Subset(dataset, range(n_test, n)) 
+    testset = torch.utils.data.Subset(dataset, range(n_test)) 
+    trainset = torch.utils.data.Subset(dataset, range(n_test, n)) 
 
-    trainloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, pin_memory=True)
-    testloader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True, pin_memory=True)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, pin_memory=True)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=True, pin_memory=True)
 
     return trainloader, testloader
