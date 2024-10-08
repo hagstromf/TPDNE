b9c18274cabd4acc0eb871a3d83893815426f13b
Fredrik Hagstrom <hagstromf@gmail.com>
2024-10-08 12:16:24 +0300

    Moved contents of utils folder to training folder

--- training/constants.py
+++ training/constants.py
@@ -4,10 +4,5 @@ import torch
 
 ROOT_DIR = Path(__file__).parent.parent
 
-if torch.cuda.is_available():
-    DEVICE = torch.device('cuda:0')
-else:
-    DEVICE = torch.device('cpu')
-
 if __name__ == '__main__':
         pass
--- training/utils.py
+++ training/utils.py
@@ -3,7 +3,7 @@ from torchvision import datasets
 from torchvision.transforms import v2, functional as F
 
 
-def load_images(path, res, batch_size=32, test_size=None):
+def load_images(path, res, test_size=None):
     transform = v2.Compose([v2.Resize((res, res)),
                             v2.ToImage(),
                             v2.ToDtype(torch.float32, scale=True),
@@ -13,15 +13,16 @@ def load_images(path, res, batch_size=32, test_size=None):
     dataset = datasets.ImageFolder(path, transform=transform)
 
     if test_size is None:
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
-        return dataloader, None
+        return dataset, None
 
     n = len(dataset) 
     n_test = int(test_size * n)  
     testset = torch.utils.data.Subset(dataset, range(n_test)) 
     trainset = torch.utils.data.Subset(dataset, range(n_test, n)) 
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, pin_memory=True)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=True, pin_memory=True)
+    return trainset, testset
 
-    return trainloader, testloader
+def unnormalize_images(imgs):
+    mean = torch.ones_like(imgs) * 0.5
+    std = torch.ones_like(imgs) * 0.5
+    return imgs * std + mean
